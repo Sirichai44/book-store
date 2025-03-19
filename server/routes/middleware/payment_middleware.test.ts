6d0c8b8788e0df7ef1cf4f67c75d8b2cf4f71b0e
Sirichai <mercier_1999@hotmail.com>
2025-03-19 15:34:39 +0700

    backend - middleware payment

---
+++ server/routes/middleware/payment_middleware.test.ts
@@ -0,0 +1,78 @@
+import { beforeEach, describe, expect, it, jest } from "bun:test"
+import type { Payment } from "../../types/payment"
+import type { NextFunction, Request, Response } from "express"
+import { validateProcessPayment } from "./payment_middleware"
+
+describe("Payment Middleware", () => {
+  let req: Partial<Request>
+  let res: Partial<Response>
+  let next: NextFunction
+
+  beforeEach(() => {
+    req = {}
+    res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    }
+    next = jest.fn()
+  })
+
+  describe("Validate Process Payment", () => {
+    it("should call next if request is valid", () => {
+      req.body = {
+        amount: 100,
+        currency: "USD",
+        items: [{ _id: "1", name: "Book", quantity: 1, price: 100 }],
+        card: { number: "1234567890123456", expiry: "01/25", cvv: "123" },
+      }
+
+      validateProcessPayment(
+        req as unknown as Request<{}, {}, Payment>,
+        res as Response,
+        next,
+      )
+
+      expect(next).toHaveBeenCalled()
+    })
+
+    it("should return 400 if request is invalid with missing field", () => {
+      req.body = {
+        amount: 100,
+        currency: "USD",
+        items: [{ _id: "1", name: "Book", quantity: 1, price: 100 }],
+        card: { number: "1234567890123456", expiry: "01/25" },
+      }
+
+      validateProcessPayment(
+        req as unknown as Request<{}, {}, Payment>,
+        res as Response,
+        next,
+      )
+
+      expect(res.status).toHaveBeenCalledWith(400)
+      expect(res.json).toHaveBeenCalledWith({
+        message: "body card cvv is Required",
+      })
+    })
+
+    it("should return 400 if request is invalid with invalid field", () => {
+      req.body = {
+        amount: 100,
+        currency: "USD",
+        items: [{ _id: "1", name: "Book", quantity: 1, price: 100 }],
+        card: { number: "1234567890123456", expiry: "01/25", cvv: "12a" },
+      }
+
+      validateProcessPayment(
+        req as unknown as Request<{}, {}, Payment>,
+        res as Response,
+        next,
+      )
+
+      expect(res.status).toHaveBeenCalledWith(400)
+      expect(res.json).toHaveBeenCalledWith({
+        message: "body card cvv is Invalid",
+      })
+    })
+  })
+})
---
+++ server/routes/middleware/payment_middleware.ts
@@ -0,0 +1,42 @@
+import type { NextFunction, Request, Response } from "express"
+import { z } from "zod"
+import type { Payment } from "../../types/payment"
+import logger from "../../config/logger.ts"
+
+const paymentSchema = z.object({
+  body: z.object({
+    amount: z.number().positive(),
+    currency: z.string(),
+    items: z.array(
+      z.object({
+        _id: z.string(),
+        name: z.string(),
+        quantity: z.number().positive(),
+        price: z.number().positive(),
+      }),
+    ),
+    card: z.object({
+      number: z.string().length(16),
+      expiry: z.string().regex(/^(0[1-9]|1[0-2])\/\d{2}$/),
+      cvv: z.string().regex(/^\d{3}$/),
+    }),
+  }),
+})
+
+export const validateProcessPayment = (
+  req: Request<{}, {}, Payment>,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { error } = paymentSchema.safeParse(req)
+  if (error) {
+    const message = error.errors
+      .map((err) => `${err.path.join(" ")} is ${err.message}`)
+      .join(", ")
+    logger.error(`Middleware: ${message}`)
+    res.status(400).json({ message })
+    return
+  }
+
+  next()
+}
